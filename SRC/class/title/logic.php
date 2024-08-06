e7b6c0e755c6bab8cf507e5af65ab340ba212b9d
YuyaShimomura <202404ap01_shimomura@x-ide.sakura.ne.jp>
2024-08-06 11:23:04 +0900

    #26080
    項目名管理更新画面で削除ボタンを押下すると項目名管理画面に遷移せずにタイトル管理画面に遷移してしまう
    187～194行目を修正

--- SRC/class/title/logic.php
+++ SRC/class/title/logic.php
@@ -11,7 +11,7 @@ function subFTitle()
         $param["sDel"] = 1;
     }
 
-    if (! $param["orderBy"]) {
+    if (!$param["orderBy"]) {
         $param["orderBy"] = 'CLASSNO,SEQNO';
         $param["orderTo"] = 'asc';
     }
@@ -31,7 +31,7 @@ function subFTitleItem()
         $param["sDel"] = 1;
     }
 
-    if (! $param["orderBy"]) {
+    if (!$param["orderBy"]) {
         $param["orderBy"] = 'CLASSNO,SEQNO';
         $param["orderTo"] = 'asc';
     }
@@ -94,7 +94,7 @@ function subFTitleEditComplete()
     if ($param["DocNo"]) {
         if ($param["seqNo"] == 0) {
             // タイトルの更新処理
-            if (! $ErrClassNo) {
+            if (!$ErrClassNo) {
                 // 更新前の情報を取得
                 $sql = fnSqlFTitleEdit($param["DocNo"]);
                 $res = mysqli_query($param["conn"], $sql);
@@ -133,9 +133,9 @@ function subFTitleEditComplete()
         }
     } else {
         $param["DocNo"] = fnNextNo('DOC');
-        if (! $param["seqNo"]) {
+        if (!$param["seqNo"]) {
 
-            if (! $ErrClassNo) {
+            if (!$ErrClassNo) {
                 $param["seqNo"] = 0;
                 $sql = fnSqlFTitleInsert($param);
                 $res = mysqli_query($param["conn"], $sql);
@@ -184,15 +184,15 @@ function subFTitleDelete()
         $res = mysqli_query($conn, $sql);
         while ($row = mysqli_fetch_array($res)) {
             $sql = fnSqlFTitleDelete($row['DOCNO']);
-            $result = mysqil_query($conn, $sql);
+            $result = mysqli_query($conn, $sql);
         }
+        $_REQUEST['act'] = 'fTitleSearch';
+        subFTitle();
     } else {
         $sql = fnSqlFTitleDelete($DocNo);
         $res = mysqli_query($conn, $sql);
+        subTitlePage1();
     }
-
-    $_REQUEST['act'] = 'fTitleSearch';
-    subFTitle();
 }
 
 //
