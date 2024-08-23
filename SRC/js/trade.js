0377e8ea4f9ac00d5058f2e078240dcbf55aa7d6
YuyaShimomura <202404ap01_shimomura@x-ide.sakura.ne.jp>
2024-08-23 11:37:37 +0900

    業者管理機能の入力チェックの共通化

--- SRC/js/trade.js
+++ SRC/js/trade.js
@@ -7,70 +7,21 @@ function fnTradeEditCheck() {
 		alert('業者名を入力してください');
 		return;
 	}
-	if (tmp.length > 100) {
-		alert('業者名は100文字以内で入力してください');
-		return;
-	}
-
-	tmp = form.nameFuri.value;
-	if (tmp.length > 100) {
-		alert('業者名（よみ）は100文字以内で入力してください');
-		return;
-	}
-
-	tmp = form.branch.value;
-	if (tmp.length > 100) {
-		alert('支店名は100文字以内で入力してください');
-		return;
-	}
-
-	tmp = form.branchFuri.value;
-	if (tmp.length > 100) {
-		alert('支店名（よみ）は100文字以内で入力してください');
-		return;
-	}
-
+	if (isLength(100, "業者名", form.name)) { return; }
+	if (isLength(100, "業者名（よみ）", form.nameFuri)) { return; }
+	if (isLength(100, "支店名", form.branch)) { return; }
+	if (isLength(100, "支店名（よみ）", form.branchFuri)) { return; }
 	tmp = form.zip.value;
 	if (tmp.length > 0 && !tmp.match(/^\d{3}(\s*|-)\d{4}$/)) {
 		alert("正しい郵便番号(***-**** 又は ******* )で\n入力してください");
 		return;
 	}
-
-	tmp = form.prefecture.value;
-	if (tmp.length > 10) {
-		alert('住所（都道府県）は10文字以内で入力してください');
-		return;
-	}
-
-	tmp = form.address1.value;
-	if (tmp.length > 100) {
-		alert('住所1（市区町村名）は100文字以内で入力してください');
-		return;
-	}
-
-	tmp = form.address2.value;
-	if (tmp.length > 100) {
-		alert('住所2（番地・ビル名）は100文字以内で入力してください');
-		return;
-	}
-
-	tmp = form.tel.value;
-	if (tmp.length > 100) {
-		alert('TELは100文字以内で入力してください');
-		return;
-	}
-
-	tmp = form.fax.value;
-	if (tmp.length > 100) {
-		alert('FAXは100文字以内で入力してください');
-		return;
-	}
-
-	tmp = form.mobile.value;
-	if (tmp.length > 100) {
-		alert('携帯電話は100文字以内で入力してください');
-		return;
-	}
+	if (isLength(10, "住所（都道府県）", form.prefecture)) { return; }
+	if (isLength(100, "住所1（市区町村名）", form.address1)) { return; }
+	if (isLength(100, "住所2（番地・ビル名）", form.address2)) { return; }
+	if (isLength(100, "TEL", form.tel)) { return; }
+	if (isLength(100, "FAX", form.fax)) { return; }
+	if (isLength(100, "携帯電話", form.mobile)) { return; }
 
 	if (confirm('この内容で登録します。よろしいですか？')) {
 		form.act.value = 'tradeEditComplete';
